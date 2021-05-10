b5f78474e3e7293f7dae55959f7b2e33fb9d9547
PKuczynski <przemek.ku2000@gmail.com>
2021-05-10 16:05:34 +0200

    Dodani asystenci

--- Matches/VolleyBall.cs
+++ Matches/VolleyBall.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using People;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Matches
 {
     class VolleyBall
     {
+        Referee AS1, AS2;
     }
 }
