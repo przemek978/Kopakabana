6cd1936799d11c37ae403dae53422da7651e5e12
PKuczynski <przemek.ku2000@gmail.com>
2021-05-10 15:44:56 +0200

    dodano druzyny do meczu

--- Matches/Match.cs
+++ Matches/Match.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kopakabana;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Matches
 {
     abstract class Match
     {
+        Team T1, T2;
     }
 }
